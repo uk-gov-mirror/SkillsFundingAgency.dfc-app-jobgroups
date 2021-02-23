023f08f14d78050f4a7f2532269399963e33c107
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2021-02-23 12:25:52 +0000

    NCSD-5120 Initial commit

---
+++ DFC.App.Jobgroups.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DFC.App.Jobgroups.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
