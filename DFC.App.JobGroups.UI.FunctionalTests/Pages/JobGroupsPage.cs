e1fffd3d92afa8391eb650e7da4fd9fd542f19e8
kadir_malik <kadir_malik@hotmail.com>
2021-04-07 15:04:55 +0100

    First commit of JobGroup UI test project

---
+++ DFC.App.JobGroups.UI.FunctionalTests/Pages/JobGroupsPage.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="JobGroupsPage.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.JobGroups.Model;
+using DFC.TestAutomation.UI.Extension;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.JobGroups.UI.FunctionalTests.Pages
+{
+    internal class JobGroupsPage
+    {
+        public JobGroupsPage(ScenarioContext context)
+        {
+            this.Context = context;
+
+            if (this.Context == null)
+            {
+                throw new NullReferenceException("The scenario context is null. The contact us home page cannot be initialised.");
+            }
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        public JobGroupsPage NavigateToJobGroupsPage()
+        {
+            this.Context.GetWebDriver().Url = this.Context.GetSettingsLibrary<AppSettings>().AppSettings.AppBaseUrl.ToString();
+            return this;
+        }
+    }
+}
---
+++ DFC.App.JobGroups.UI.FunctionalTests/StepDefinitions/ValidationSteps.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="ValidationSteps.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.App.JobGroups.Model;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System.Globalization;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.JobGroups.UI.FunctionalTests.StepDefinitions
+{
+    [Binding]
+    internal class ValidationSteps
+    {
+        public ValidationSteps(ScenarioContext context)
+        {
+            this.Context = context;
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        [Then(@"I am on the (.*) page")]
+        public void ThenIAmOnThePage(string pageName)
+        {
+            By locator = null;
+
+            switch (pageName.ToLower(CultureInfo.CurrentCulture))
+            {
+                case "job group: nurses":
+                    locator = By.CssSelector("h1");
+                    break;
+
+                default:
+                    locator = By.CssSelector("h1");
+                    break;
+            }
+
+            this.Context.GetHelperLibrary<AppSettings>().WebDriverWaitHelper.WaitForElementToContainText(locator, pageName);
+        }
+
+        [Then(@"the (.*) information is displayed")]
+        public void ThenTheJobGrowthInformationIsDisplayed(string LMI)
+        {
+            By locator = null;
+
+            switch (LMI.ToLower(CultureInfo.CurrentCulture))
+            {
+                case "job growth":
+                    locator = By.CssSelector(".dfc-app-lmi-panel.panel-green");
+                    break;
+
+                case "qualifications":
+                    locator = By.CssSelector(".dfc-app-lmi-panel.panel-green.panel-qualifications");
+                    break;
+
+                case "regional":
+                    locator = By.XPath("//*[@id='main-content']/div/div/div/div[1]/div/table[2]/thead/tr/th[1]");
+                    break;
+
+                case "industry":
+                    locator = By.XPath("//*[@id='main-content']/div/div/div/div[1]/div/table[1]/thead/tr/th[1]");
+                    break;
+
+                default:
+                    locator = By.CssSelector("h1");
+                    break;
+            }
+
+            this.Context.GetHelperLibrary<AppSettings>().WebDriverWaitHelper.WaitForElementToBeDisplayed(locator);
+        }
+
+    }
+}
